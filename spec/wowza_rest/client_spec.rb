56c3fedd54f246a08b49d8de4a6b62b0e3066585
Hazem Taha <thatskan@gmail.com>
2017-07-01 11:30:23 +0200

    refactor client_spec

--- spec/wowza_rest/client_spec.rb
+++ spec/wowza_rest/client_spec.rb
@@ -2,10 +2,10 @@ require 'spec_helper'
 
 RSpec.describe WowzaRest::Client do
   let(:client) do
-    WowzaRest::Client.new(host: '127.0.0.1',
-                          port: '8087',
-                          username: ENV.fetch('WOWZA_USERNAME'),
-                          password: ENV.fetch('WOWZA_PASSWORD'))
+    described_class.new(host: '127.0.0.1',
+                        port: '8087',
+                        username: ENV.fetch('WOWZA_USERNAME'),
+                        password: ENV.fetch('WOWZA_PASSWORD'))
   end
 
   it 'has a default server name' do
