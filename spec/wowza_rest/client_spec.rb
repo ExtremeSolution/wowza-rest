fe70e802f89bc8cd1341f337fe3223931024ad7a
Hazem Taha <thatskan@gmail.com>
2017-07-02 12:44:19 +0200

    refactor client_spec

--- spec/wowza_rest/client_spec.rb
+++ spec/wowza_rest/client_spec.rb
@@ -12,31 +12,35 @@ RSpec.describe WowzaRest::Client do
                         username: 'username', password: 'wrongpassword')
   end
 
-  it 'has a default server name' do
-    expect(client.server_name).to eq '_defaultServer_'
+  context 'when instantiating a new object' do
+    it 'has a default server name' do
+      expect(client.server_name).to eq '_defaultServer_'
+    end
+
+    it 'has a default api version' do
+      expect(client.api_version).to eq 'v2'
+    end
+
+    it 'has a default vhost' do
+      expect(client.vhost).to eq '_defaultVHost_'
+    end
+
+    it 'has a valid base uri' do
+      expect(client.base_uri).to(
+        eq '127.0.0.1:8087/v2/servers/_defaultServer_/vhosts/_defaultVHost_'
+      )
+    end
+
+    it 'has a valid connection object' do
+      expect(client.connection).to be_an_instance_of(WowzaRest::Connection)
+    end
   end
 
-  it 'has a default api version' do
-    expect(client.api_version).to eq 'v2'
-  end
-
-  it 'has a default vhost' do
-    expect(client.vhost).to eq '_defaultVHost_'
-  end
-
-  it 'has a valid base uri' do
-    expect(client.base_uri).to(
-      eq '127.0.0.1:8087/v2/servers/_defaultServer_/vhosts/_defaultVHost_'
-    )
-  end
-
-  it 'has a valid connection object' do
-    expect(client.connection).to be_an_instance_of(WowzaRest::Connection)
-  end
-
-  it 'responds with 401 when using incorrect creds',
-     vcr: { cassette_name: 'all_applications_unauthrized' } do
-    response = wrong_client.applications
-    expect(response['code']).to eq('401')
+  context 'when it has an invalid creds' do
+    it 'responds with 401',
+       vcr: { cassette_name: 'all_applications_unauthrized' } do
+      response = wrong_client.applications
+      expect(response['code']).to eq('401')
+    end
   end
 end
