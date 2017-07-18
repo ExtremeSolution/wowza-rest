f8413281f12d2f8988067c259b58eb1eff1199f6
Hazem Taha <thatskan@gmail.com>
2017-07-18 14:11:58 +0200

    fix rails active support conflict with gem string monkey patching, and instead monkey patching using a module

--- lib/wowza_rest.rb
+++ lib/wowza_rest.rb
@@ -1,7 +1,8 @@
 require 'wowza_rest/version'
 require 'wowza_rest/client'
-require 'wowza_rest/ext/string'
+require 'wowza_rest/core_ext/string/inflections'
 require 'json'
 
 module WowzaRest
+  String.include WowzaRest::CoreExt::String::Inflections
 end
