55054c83d5090160ddbc40f57a90772c8c816a85
Joe <jswilliam93@gmail.com>
2017-08-17 17:00:23 +0200

    fix based on unit test

--- lib/wowza_rest/smils.rb
+++ lib/wowza_rest/smils.rb
@@ -11,25 +11,29 @@ module WowzaRest
             response.parsed_response['smilFiles']
                       .map { |e| WowzaRest::Data::SMILShort.new(e) }
         end
-  
+
         def get_smil(smil_name)
             response = connection.request(:get, "/smilfiles/#{smil_name}")
             return unless response.code == 200
-            WowzaRest::Data::SMIL.new(response.parsed_response)  
-        end 
+            WowzaRest::Data::SMIL.new(response.parsed_response)
+        end
 
         def create_smil(smil_name, smil_body)
             unless smil_name.is_a?(String)
                 raise WowzaRest::Errors::InvalidArgumentType,
                     "First argument expected to be String got #{smil_name.class}"
             end
-            connection.request(:post, "/smilfiles/#{smil_name}", body: smil_body.to_json)            
+            connection.request(:post, "/smilfiles/#{smil_name}", body: smil_body.to_json)
         end
 
         def update_smil(smil_name, smil_body)
+          unless smil_name.is_a?(String)
+              raise WowzaRest::Errors::InvalidArgumentType,
+                  "First argument expected to be String got #{smil_name.class}"
+          end
             connection.request(:put, "/smilfiles/#{smil_name}", body: smil_body.to_json)['success']
         end
-    
+
         def delete_smil(smil_name)
             unless smil_name.is_a?(String)
                 raise WowzaRest::Errors::InvalidArgumentType,
@@ -37,7 +41,6 @@ module WowzaRest
             end
             connection.request(:delete, "/smilfiles/#{smil_name}")['success']
         end
-      
+
     end
   end
-  
